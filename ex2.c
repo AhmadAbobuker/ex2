c60496bd5dd12a4e76d2351f089b9b0d93d740e1
ahmad <ahmadabobuker15@outlook.com>
2025-04-10 22:04:32 +0300

    No changes made in the last update

--- ex2.c
+++ ex2.c
@@ -3,19 +3,15 @@ Name:ahmad abu bakr
 ID:213109192
 Assignment: ex2
 *******************/
-
-
-
-
 #include <stdio.h>
 
 int main() {
+    int digitscounter=0 ;
     int Equal = 0;
-	int sumcheer = 0;
-	int sumsmile = 0;
+    int sumcheer = 0;
+    int sumsmile = 0;
     int powerof10 = 1;
-    int digitscounter = 0;
-	int try = -1;
+    int try = -1;
     int ErrorCounter = 0, SmilePosition = 0, CheerPosition = 0, CommaPosition = 0, NumbersValidater = 0, EndOfLinePosition = 0, UpNormality = 0;
     int number10, sum10 = 0;
     int n1, n2 = 0;
@@ -23,7 +19,6 @@ int main() {
     int temp2;
     int primecounter = 0;
     int n, i;
-    int num1 = 0, num2 = 0;
     printf("Choose an option:\n\n%s%s%s%s%s%s%s",
         "    1. Happy Face\n\n",
         "    2. Balanced Number\n\n",
@@ -42,7 +37,7 @@ int main() {
     }
 
     switch (option) {
-    case 1:
+    case 1: {
         printf("Happy Face\n");
         char eyes, mouth, nose;
         printf("Enter the Eyes, Nose and Mouth of the Happy Face:\n");
@@ -72,7 +67,8 @@ int main() {
         }
         printf("/");
         break;
-    case 2:
+    }
+    case 2: {
         printf("Enter a number:");
         int number, numbertemp, sum1 = 0, numbertemp2 = 0, sum2 = 0;
         int digitscounter = 1;
@@ -123,7 +119,8 @@ int main() {
             printf("This number isn't balanced and destroys harmony.\n");
         }
         break;
-    case 3:
+    }
+    case 3: {
         printf("Enter a number:");
         scanf("%d", &number10);
         while (number10 < 0) {
@@ -143,9 +140,10 @@ int main() {
             printf("This number does not share.\n");
         }
         break;
-    case 4:
+    }
+    case 4: {
         printf("Enter a number:");
-        digitscounter = 1;
+        int digitscounter = 1;
         scanf("%d", &n1);
         temp = n1;
         temp2 = n1;
@@ -196,7 +194,8 @@ int main() {
             printf("The circle remains incomplete.\n");
         }
         break;
-    case 5:
+    }
+    case 5: {
         printf("Enter a number: ");
         scanf("%d", &n);
         while (n <= 0) {
@@ -215,7 +214,6 @@ int main() {
                     current /= 10;
                 }
 
-
                 if (sum == 1) {
                     printf("%d ", i);
                     break;
@@ -228,22 +226,19 @@ int main() {
             }
         }
         break;
-    case 6:
+    }
+    case 6: {
         printf("Enter a smile and cheer number:\n");
         char Phrase[1000];
-        //we need the user to give us his input and to reEnter it if its not in a certin style for that we need a while loop
-
         while ((ErrorCounter != 4) || (NumbersValidater != 2) || (UpNormality != 0)||(Equal!=0)) {
             if (try > 0) {
                 printf("Only 2 different positive numbers in the given format are allowed for the festival, please try again:\n");
             }
-            //filing the array with garbage values to avoid random vlaues so we can manipulate the string each time it restarts
             for (int i = 0; i < 1000; i++) {
                 Phrase[i] = '\0';
             }
-            //Restarting the Counters for each new input after reset
             ErrorCounter = 0;
-			SmilePosition = 999;// it being 999 is a flag for the machine to know that it has not been set yet , it solves the problem of 0 being a valid value
+            SmilePosition = 999;
             CheerPosition = 0;
             CommaPosition = 0;
             NumbersValidater = 0;
@@ -251,81 +246,52 @@ int main() {
             UpNormality = 0;
             digitscounter = 0;
             powerof10 = 1;
-			Equal = 0;
-			sumsmile = 0;
-			sumcheer = 0;
-            //we need to store the garbage value \n before any input
-            //Phrase[0]=getchar() ;
-            //Asking user for input till he clicks Enter
+            Equal = 0;
+            sumsmile = 0;
+            sumcheer = 0;
             for (int i = 0; i < 1000; i++) {
                 Phrase[i] = getchar();
-                //breaking mechansim in case of Enter
                 if (Phrase[i] == '\n') {
-                    //saving the EndOfLinePosition for later
                     EndOfLinePosition = i;
                     break;
                 }
             }
-            //The checking mechansim should recgonize a pattren within the string so we teach it to recgonize "smile:"
-            //and recgonize "cheer:" and "," ,and there order in the array
-
-            //smile: detction
             for (int i = 0; i < 1000; i++) {
                 if ((Phrase[i] == 's') && (Phrase[i + 1] == 'm') && (Phrase[i + 2] == 'i') && (Phrase[i + 3] == 'l') && (Phrase[i + 4] == 'e') && (Phrase[i + 5] == ':')) {
-                    //here we introduce the Error counter each recgonition earns us a point
                     ErrorCounter++;
-                    //here we introduce the Position saver to validate order
                     SmilePosition = i;
                 }
             }
-            //comma detction
             for (int i = 0; i < 1000; i++) {
                 if ((Phrase[i] == ',')) {
                     ErrorCounter++;
                     CommaPosition = i;
                 }
             }
-            //cheer:detction
             for (int i = 0; i < 1000; i++) {
                 if ((Phrase[i] == 'c') && (Phrase[i + 1] == 'h') && (Phrase[i + 2] == 'e') && (Phrase[i + 3] == 'e') && (Phrase[i + 4] == 'r') && (Phrase[i + 5] == ':')) {
                     ErrorCounter++;
                     CheerPosition = i;
                 }
             }
-            //Order validation mechansim
             if ((CommaPosition > SmilePosition) && (CommaPosition < CheerPosition)) {
-
                 ErrorCounter++;
-
             }
-            //validation of what is in bettwen the "smile:" and "," , and "cheer:" and "\n" ;
-            // we need the machine to check what is in bettwen only when it recgonizes the format so it dosent check randomly
-            //thus the ErrorCounter must be 4 then and only then what is in bettwen matters
-
-            //"smile:" and "," *in bettwen the array only can hold numbers and " "
             if (ErrorCounter == 4) {
                 for (int i = SmilePosition + 6; i < CommaPosition; i++) {
                     if ((Phrase[i] != '0') && (Phrase[i] != '1') && (Phrase[i] != '2') && (Phrase[i] != '3') && (Phrase[i] != '4') && (Phrase[i] != '5') && (Phrase[i] != '6') && (Phrase[i] != '7') && (Phrase[i] != '8') && (Phrase[i] != '9') && (Phrase[i] != ' ')) {
-                        //in Such a case the space bettwen a comma and smile: is unknown
-                        //but what is known is the format we need which is only numbers and spaces
-                        //so for each wrong char in the array we take 1 from the ErrorCounter so the machine flags an Error
-                        // in case of succuess the ErrorCounter remains unchanged
                         ErrorCounter--;
                     }
                 }
             }
-            //Edge case would be if there is no numbers in bettwen since spaces are allowed so we fix that by detction for
-            //numbers after confirming the format so we introduce a NumbersValidater
             if (ErrorCounter == 4) {
                 for (int i = SmilePosition + 6; i < CommaPosition; i++) {
                     if ((Phrase[i] == '0') || (Phrase[i] == '1') || (Phrase[i] == '2') || (Phrase[i] == '3') || (Phrase[i] == '4') || (Phrase[i] == '5') || (Phrase[i] == '6') || (Phrase[i] == '7') || (Phrase[i] == '8') || (Phrase[i] == '9')) {
                         NumbersValidater++;
-                        //since all is requierd after confirming the format is at least one number exists we close the loop
                         break;
                     }
                 }
             }
-            //we do the same for "cheer:" and "\n"
             if (ErrorCounter == 4) {
                 for (int i = CheerPosition + 6; i < EndOfLinePosition; i++) {
                     if ((Phrase[i] != '0') && (Phrase[i] != '1') && (Phrase[i] != '2') && (Phrase[i] != '3') && (Phrase[i] != '4') && (Phrase[i] != '5') && (Phrase[i] != '6') && (Phrase[i] != '7') && (Phrase[i] != '8') && (Phrase[i] != '9') && (Phrase[i] != ' ')) {
@@ -338,126 +304,99 @@ int main() {
                 for (int i = CheerPosition + 6; i < EndOfLinePosition; i++) {
                     if ((Phrase[i] == '0') || (Phrase[i] == '1') || (Phrase[i] == '2') || (Phrase[i] == '3') || (Phrase[i] == '4') || (Phrase[i] == '5') || (Phrase[i] == '6') || (Phrase[i] == '7') || (Phrase[i] == '8') || (Phrase[i] == '9')) {
                         NumbersValidater++;
-                        //since all is requierd after confirming the format is at least one number exists we close the loop
                         break;
                     }
                 }
             }
-            //The machine now can recgonize this line and patterns withing the line  "Smile:integer , cheer:integer" in the string Phrase
-            // A complication would be if there is more that exists
-            // the solution would be for the machine to flag it as an error if something more than the trash we inputed exists
-            //so we need a final check for the string before the line starts and after it ends
-
-            //before the line starts check
             if ((ErrorCounter == 4) && (NumbersValidater == 2)) {
                 for (int i = 0; i < SmilePosition; i++) {
                     if ((Phrase[i] == '\0') || (Phrase[i] == '\n')) {
 
                     }
                     else {
-                        //here we introduce for UpNormality check within the string if it exist it will flag it
                         UpNormality++;
                     }
                 }
             }
-            //after the line ends check
             if ((ErrorCounter == 4) && (NumbersValidater == 2)) {
                 for (int i = EndOfLinePosition + 1; i < 1000; i++) {
                     if ((Phrase[i] == '\0') || (Phrase[i] == '\n')) {
 
                     }
                     else {
-                        //here we introduce for UpNormality check within the string if it exist it will flag it
                         UpNormality++;
                     }
                 }
             }
-            //we need to check if the numbers after smile and after cheer are the same in value
-            //if they are we need to flag an error and return the user to reEnter the values.
-		    //we need to convert the string to an intger so we can compare them
             if ((ErrorCounter == 4)&&(NumbersValidater==2)&&(UpNormality==0)) {
                 for (int i = SmilePosition + 6; i < CommaPosition; i++) {
                     if ((Phrase[i] == '0') || (Phrase[i] == '1') || (Phrase[i] == '2') || (Phrase[i] == '3') || (Phrase[i] == '4') || (Phrase[i] == '5') || (Phrase[i] == '6') || (Phrase[i] == '7') || (Phrase[i] == '8') || (Phrase[i] == '9')) {
                         digitscounter++;
                     }
                 }
-					for (int i = 0; i < digitscounter-1; i++) {
-						//constructing power of 10
-						powerof10 = powerof10 * 10;
-					}
-					//we know how many are they now we need to run into them 1 by 1 and start multiplying them by 10 to the power of digitscounter-1
-                    for (int i = SmilePosition + 6; i < CommaPosition; i++) {
-                        if ((Phrase[i] == '0') || (Phrase[i] == '1') || (Phrase[i] == '2') || (Phrase[i] == '3') || (Phrase[i] == '4') || (Phrase[i] == '5') || (Phrase[i] == '6') || (Phrase[i] == '7') || (Phrase[i] == '8') || (Phrase[i] == '9')) {
-                            sumsmile = sumsmile + (Phrase[i] - 48) * powerof10;
-                            powerof10 = powerof10 / 10;
-                        }
-				}
-
+                for (int i = 0; i < digitscounter-1; i++) {
+                    powerof10 = powerof10 * 10;
+                }
+                for (int i = SmilePosition + 6; i < CommaPosition; i++) {
+                    if ((Phrase[i] == '0') || (Phrase[i] == '1') || (Phrase[i] == '2') || (Phrase[i] == '3') || (Phrase[i] == '4') || (Phrase[i] == '5') || (Phrase[i] == '6') || (Phrase[i] == '7') || (Phrase[i] == '8') || (Phrase[i] == '9')) {
+                        sumsmile = sumsmile + (Phrase[i] - 48) * powerof10;
+                        powerof10 = powerof10 / 10;
+                    }
+                }
             }
-			digitscounter = 0;
-			powerof10 = 1;
-			//we need to do the same for cheer
-			if ((ErrorCounter == 4) && (NumbersValidater == 2) && (UpNormality == 0)) {
-				for (int i = CheerPosition + 6; i < EndOfLinePosition; i++) {
-					if ((Phrase[i] == '0') || (Phrase[i] == '1') || (Phrase[i] == '2') || (Phrase[i] == '3') || (Phrase[i] == '4') || (Phrase[i] == '5') || (Phrase[i] == '6') || (Phrase[i] == '7') || (Phrase[i] == '8') || (Phrase[i] == '9')) {
-						digitscounter++;
-					}
-				}
-				for (int i = 0; i < digitscounter - 1; i++) {
-					//constructing power of 10
-					powerof10 = powerof10 * 10;
-				}
-				//we know how many are they now we need to run into them 1 by 1 and start multiplying them by 10 to the power of digitscounter-1
-				for (int i = CheerPosition + 6; i < EndOfLinePosition; i++) {
-					if ((Phrase[i] == '0') || (Phrase[i] == '1') || (Phrase[i] == '2') || (Phrase[i] == '3') || (Phrase[i] == '4') || (Phrase[i] == '5') || (Phrase[i] == '6') || (Phrase[i] == '7') || (Phrase[i] == '8') || (Phrase[i] == '9')) {
-						sumcheer = sumcheer + (Phrase[i] - 48) * powerof10;
-						powerof10 = powerof10 / 10;
-					}
-				}
-                //here we need to check if the numbers are equal or not
-                //if they are equal we need to flag an error and return the user to reEnter the values.
-                //so we intrduce Equal checker 0 false 1 true
+            digitscounter = 0;
+            powerof10 = 1;
+            if ((ErrorCounter == 4) && (NumbersValidater == 2) && (UpNormality == 0)) {
+                for (int i = CheerPosition + 6; i < EndOfLinePosition; i++) {
+                    if ((Phrase[i] == '0') || (Phrase[i] == '1') || (Phrase[i] == '2') || (Phrase[i] == '3') || (Phrase[i] == '4') || (Phrase[i] == '5') || (Phrase[i] == '6') || (Phrase[i] == '7') || (Phrase[i] == '8') || (Phrase[i] == '9')) {
+                        digitscounter++;
+                    }
+                }
+                for (int i = 0; i < digitscounter - 1; i++) {
+                    powerof10 = powerof10 * 10;
+                }
+                for (int i = CheerPosition + 6; i < EndOfLinePosition; i++) {
+                    if ((Phrase[i] == '0') || (Phrase[i] == '1') || (Phrase[i] == '2') || (Phrase[i] == '3') || (Phrase[i] == '4') || (Phrase[i] == '5') || (Phrase[i] == '6') || (Phrase[i] == '7') || (Phrase[i] == '8') || (Phrase[i] == '9')) {
+                        sumcheer = sumcheer + (Phrase[i] - 48) * powerof10;
+                        powerof10 = powerof10 / 10;
+                    }
+                }
                 if ((sumsmile == sumcheer)) {
                     Equal++;
-
                 }
-			}
-            //The checks are over The pattren is set for the machine to detect if it pases all the flags then its valid
+            }
             try++;
         }
 
         int maxnumber = 0;
-		printf("Enter the maximum number of the festival:\n");
-		scanf("%d", &maxnumber);
-		while (maxnumber < 0) {
-			printf("Only positive maximum number is allowed, please try again:\n");
-			scanf("%d", &maxnumber);
-		}
-
-
+        printf("Enter the maximum number of the festival:\n");
+        scanf("%d", &maxnumber);
+        while (maxnumber < 0) {
+            printf("Only positive maximum number is allowed, please try again:\n");
+            scanf("%d", &maxnumber);
+        }
 
         for (int i = 1; i <= maxnumber; i++)
         {
-			if (i % sumsmile == 0 && i % sumcheer == 0)
-			{
-				printf("Festival!\n");
-			}
-			else if (i % sumsmile == 0)
-			{
-				printf("Smile!\n");
-			}
-			else if (i % sumcheer == 0)
-			{
-				printf("Cheer!\n");
-			}
-			else
-			{
-				printf("%d\n", i);
-			}
-
-
+            if (i % sumsmile == 0 && i % sumcheer == 0)
+            {
+                printf("Festival!\n");
+            }
+            else if (i % sumsmile == 0)
+            {
+                printf("Smile!\n");
+            }
+            else if (i % sumcheer == 0)
+            {
+                printf("Cheer!\n");
+            }
+            else
+            {
+                printf("%d\n", i);
+            }
         }
         break;
+    }
     case 7:
         printf("Thank you for your journey through Numerial!\n");
         break;
@@ -465,4 +404,3 @@ int main() {
 
     return 0;
 }
-
